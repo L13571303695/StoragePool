52fbfd7a417a6a6330df29cc2b3596cc713572f8
caolinlin <417141344@qq.com>
2019-06-17 09:17:38 +0800

    添加test工程至git

---
+++ idea-git-test/src/com/itheima/test/HelloGit.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
